7d0353b451a5e495bd466c89605a6c81b944522e
jhebert <jhebert@b6d15d3a-9b2f-0410-be8d-89e6da484874>
2007-05-09 01:33:03 +0000

    Empty files for what I will be writing tomorrow.

---
+++ fetcher/bayes-classifier.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python2.4
+
+
+
+""" This class will classify inputs according to the
+bayes learner model output from bayes-learner. """
+
+__author__ = 'jhebert@cs.washington.edu (Jack Hebert)'
+
---
+++ fetcher/bayes-learner.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python2.4
+
+
+
+""" This class will learn a bayes classifier given a
+set of examples. It will be defined as working over
+textual corpora. """
+
+__author__ = 'jhebert@cs.washington.edu (Jack Hebert)'
+
