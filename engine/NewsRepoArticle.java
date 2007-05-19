8d6ac961415c17271c90a880e300df9af1227ac0
jhebert <jhebert@b6d15d3a-9b2f-0410-be8d-89e6da484874>
2007-05-19 21:07:45 +0000

    This files compile but have not been tested. They are utilities to read through the news repo build by the python script.

---
+++ engine/NewsRepoArticle.java
@@ -0,0 +1,34 @@
+/*
+ * NewsTerp Engine - We report.  You decipher.
+ * copyright (c) 2007 Colin Bayer, Jack Hebert
+ *
+ * CSE 472 Spring 2007 final project
+ */
+
+
+public class NewsRepoArticle {
+
+    private String url;
+    private String articleText;
+
+    public NewsRepoArticle(String line) {
+	int index = line.indexOf('\t');
+	if(index == -1) {
+	    this.url = "";
+	    this.articleText = "";
+	} else {
+	    this.url = line.substring(0, index);
+	    this.url = line.substring(index+1);
+	}
+    }
+
+    public String getUrl() {
+	return this.url;
+    }
+
+    public String getArticle() {
+	return this.articleText;
+    }
+
+
+}
---
+++ engine/NewsRepoReader.java
@@ -0,0 +1,73 @@
+/*
+ * NewsTerp Engine - We report.  You decipher.
+ * copyright (c) 2007 Colin Bayer, Jack Hebert
+ *
+ * CSE 472 Spring 2007 final project
+ */
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class NewsRepoReader {
+
+    private ArrayList<String> newsFiles;
+    private int fileIndex;
+    private BufferedReader reader;
+
+    public NewsRepoReader(String filePath) {
+	this.newsFiles = new ArrayList<String>();
+	this.AddFiles(filePath);
+	this.fileIndex = 0;
+	try{
+	    if(this.newsFiles.size() > 0) {
+		this.reader = new BufferedReader(new FileReader(
+			this.newsFiles.get(this.fileIndex)));
+	    } else {
+		this.reader = null;
+	    }
+	} catch(Exception e) {
+	    System.out.println(e.toString());
+	}
+    }
+
+    private void AddFiles(String path) {
+	File input = new File(path);
+	if(! input.isDirectory()) {
+	    this.newsFiles.add(path);
+	} else {
+	    String[] children = input.list();
+	    for(int i=0; i<children.length; i++) {
+		String name = children[i];
+		if(!name.startsWith("."))
+		    this.AddFiles(name);
+	    }
+	}
+    }
+
+    public NewsRepoArticle GetNextArticle(){
+	// read the next line of the current file
+	// if at the end of file, move to the next file
+	// if at end of list of files, return null;
+	try {
+	    String line = this.reader.readLine();
+	    while(line == null) {
+		if(this.fileIndex < this.newsFiles.size()) {
+		    this.fileIndex += 1;
+		    this.reader = new BufferedReader(new FileReader(
+								    this.newsFiles.get(this.fileIndex)));
+		} else {
+		    return null;
+		}
+		line = this.reader.readLine();
+	    }
+	    return new NewsRepoArticle(line);
+	} catch(Exception e) {
+	    System.out.println(e.toString());
+	}
+	return null;
+    }
+
+}
