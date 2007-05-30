cb0821b3ad1a6a63404d804c2526fb43ab50da51
prostet <prostet@b6d15d3a-9b2f-0410-be8d-89e6da484874>
2007-05-30 04:12:14 +0000

    fixxed a missing file...

---
+++ engine/RelationSet.java
@@ -0,0 +1,35 @@
+/*
+ * NewsTerp Engine - We report.  You decipher.
+ * copyright (c) 2007 Colin Bayer, Jack Hebert
+ *
+ * CSE 472 Spring 2007 final project
+ */
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class RelationSet {
+	public RelationSet(String aID) {
+		mID = aID;
+		mRelations = new HashSet<Relation>();
+	}
+
+	public void add(Relation aRel) {
+		mRelations.add(aRel);
+	}
+
+	public String toSerialRep() {
+		String rv = "set \"" + mID + "\";\n";
+
+		for (Relation r : mRelations) {
+			rv += r.toSerialRep();
+		}
+
+		rv += "endset;\n";
+
+		return rv;
+	}
+
+	private Set<Relation> mRelations;
+	private String mID;
+}
